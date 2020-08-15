bdd168ac63d974dbd8abb24f911b06d577f7bb34
Suxen11 <69393818+Suxen11@users.noreply.github.com>
2020-08-15 10:01:51 -0700

    Segundo commit

---
+++ SeleniumAutomation/src/test/java/com/javabasic/Datatypes.java
@@ -0,0 +1,53 @@
+package com.javabasic;
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		/*
+		 * Datos Numericos
+		 */
+//		byte x;
+//		short y;
+//		int z;
+//		long a;
+//		
+//		x=127;
+//		y=32767;
+//		z=2147483647;
+//		a=9223372036854775807L;
+//		
+//		System.out.println(x);
+		
+		/*
+		 * Numericos Reales
+		 */
+		
+//		float base;
+//		double altura;
+//		
+//		base = 30.5F;
+//		altura = 40.76;
+//		
+//		System.out.println(base);
+//		
+//		char letra1;
+//		char letra2;
+//		String frase;
+//		
+//		letra1 = 'a';
+//		letra2 = 'x';
+//		frase = "Hola Mundo";
+//		
+//		System.out.println(frase);
+		
+		boolean  volar;
+		
+		volar = false;
+		
+		System.out.println("Tu puedes volar?" + volar);
+	}
+
+}
---
+++ SeleniumAutomation/src/test/java/com/javabasic/OperatorsStatements.java
@@ -0,0 +1,52 @@
+package com.javabasic;
+
+public class OperatorsStatements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/*
+		 * Operadores Aritmeticos
+		 */
+//		
+//	int x = 1;
+//	int y = 2;
+//	int suma;
+//	
+//	suma = x+y;
+//	
+//	System.out.println(suma);
+		
+		/*
+		 * Operadores Unarios
+//		 */
+//		int valor1 = 3;
+//		int incrementar = ++valor1;
+//		
+//		System.out.println(incrementar);
+//		
+		/*
+		 * Operacions Racionalies
+		 */
+//		
+//		int num1 = 3;
+//		int num2 = 4;
+//		if (num1 == num2);
+//			System.out.println("Los numeros son iguales");
+//				else  {
+//		System.out.println("Los numero no son iguales");
+		
+		/*
+		 * Operaciones Condicionales
+		 */
+		
+		int num1 = 10;
+		int num2 =  4;
+		int num3 =  6;
+		int num4 = 11;
+		if (num1 >= 10 || num2 >10)
+	}
+	
+}
+	
+		
